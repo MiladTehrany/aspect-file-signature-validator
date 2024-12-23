8e5a48bb3a5ffd498d5adcbed5b261e64453c67c
Milad Tehrany <mhtxpress7@gmail.com>
2024-12-23 13:49:16 +0330

    Update: Structure API

--- jakarta-validator/src/main/java/com/signature/ExampleServlet.java
+++ jakarta-validator/src/main/java/com/signature/ExampleServlet.java
@@ -1,18 +1,31 @@
 package com.signature;
 
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/file")
+@RequestScoped
+@Path("/storage")
 public class ExampleServlet {
 
     @POST
+    @Path("upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response method(@Context HttpServletRequest request) {
         System.out.println("File Received");
         return Response.ok("File Received").build();
+    }
+
+    @GET
+    @Path("/search")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getFile(@Context HttpServletRequest request) {
+        System.out.println("Show Files");
+        return Response.ok("Show Files").build();
     }
 
 }
